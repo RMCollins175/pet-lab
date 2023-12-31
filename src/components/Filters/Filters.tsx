e4a96d6a1d72e9b49184724a275dc2eef3e8f942
Rory <rorycollins@me.com>
2023-12-31 17:12:32 +0000

    fix responsive styling

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -10,14 +10,21 @@ export const Filters = ({ handleSearch, loading }: FiltersProps) => {
     <div className="filters-container">
       <form onSubmit={handleSearch} className="filters">
         <label className="filter" data-testid="tag-label">
-          Tag: <input type="text" name="tag" />
+          Tag: <input className="inputs" type="text" name="tag" />
         </label>
         <label className="filter" data-testid="price-label">
-          Price: <input type="number" name="price" min="1" max="1000" />
+          Price:{" "}
+          <input
+            className="inputs"
+            type="number"
+            name="price"
+            min="1"
+            max="1000"
+          />
         </label>
         <label className="filter" data-testid="subscription-label">
           Subscription:
-          <select name="subscription">
+          <select name="subscription" className="inputs">
             <option></option>
             <option value="true">Yes</option>
             <option value="false">No</option>
@@ -27,16 +34,14 @@ export const Filters = ({ handleSearch, loading }: FiltersProps) => {
           type="submit"
           className="search-button"
           disabled={loading}
-          data-testid="search-button"
-        >
+          data-testid="search-button">
           Search
         </button>
         <input
           type="reset"
           className="search-button"
           value="Reset"
-          data-testid="reset-button"
-        ></input>
+          data-testid="reset-button"></input>
       </form>
     </div>
   );
