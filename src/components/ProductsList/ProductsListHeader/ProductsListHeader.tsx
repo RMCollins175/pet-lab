6a2b7327e1278c913c39941cb64bea2dce235911
Rory <rorycollins@me.com>
2024-01-06 10:26:41 +0000

    Remove unused dependencies and update UI components

--- src/components/ProductsList/ProductsListHeader/ProductsListHeader.tsx
+++ src/components/ProductsList/ProductsListHeader/ProductsListHeader.tsx
@@ -1,24 +1,25 @@
 export const ProductsListHeader = () => {
   return (
-    <li
-      className="grid justify-center grid-cols-[0.2fr_1.5fr_0.5fr_0.5fr] p-4 border-b border-gray-300 cursor-pointer"
-      data-testid="product-list-header">
+    <li className="grid justify-center grid-cols-[0.2fr_1.5fr_0.5fr_0.5fr] p-4 border-b border-gray-300 cursor-pointer">
       <div></div>
       <span
         className="flex items-center justify-center"
-        data-testid="product-name-header">
+        aria-label="Title"
+        role="columnheader">
         <strong>Title</strong>
       </span>
 
       <span
         className="flex items-center justify-center"
-        data-testid="product-price-header">
+        aria-label="Price"
+        role="columnheader">
         <strong>Price</strong>
       </span>
 
       <span
         className="flex items-center justify-center"
-        data-testid="subscription-discount-header">
+        aria-label="Discount"
+        role="columnheader">
         <strong>Discount</strong>
       </span>
     </li>
